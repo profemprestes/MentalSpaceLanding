6f77fb0823566d0b3e5e22fc6785efff221531eb
Profe Matias Prestes <profematiasprestes@gmail.com>
2025-04-25 20:54:49 -0300

    refactor(tailwind): move custom colors into extend block
    This change improves maintainability by organizing custom colors under the `extend` block in the Tailwind configuration, aligning with best practices for theme customization.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,21 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-		mytheme: {
-			"primary": "#9400ff",
-			"secondary": "#ff0000",
-			"accent": "#7c3a00",
-			"neutral": "#1b2d3e",
-			"base-100": "#fffdff",
-			"info": "#00d5ff",
-			"success": "#94c600",
-			"warning": "#c32600",
-			"error": "#ff8783",
-		},
-	},
-	plugins: [
-		require('tailwindcss-animate'),
-	],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#9400ff',
+        secondary: '#ff0000',
+        accent: '#7c3a00',
+        neutral: '#1b2d3e',
+        'base-100': '#fffdff',
+        info: '#00d5ff',
+        success: '#94c600',
+        warning: '#c32600',
+        error: '#ff8783',
+      },
+    },
+  },
+  plugins: [
+    require('tailwindcss-animate'),
+  ],
 }
